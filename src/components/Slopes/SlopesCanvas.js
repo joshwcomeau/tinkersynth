f7f3830f49a3e6f3f17c53c811d36620b2bbab37
Joshua Comeau <joshwcomeau@gmail.com>
2018-12-13 08:47:50 -0500

    Refactor offscreenCanvas code

--- src/components/Slopes/SlopesCanvas.js
+++ src/components/Slopes/SlopesCanvas.js
@@ -6,7 +6,7 @@ import Canvas from '../Canvas';
 
 import transformParameters from './Slopes.params';
 
-import Worker from './Slopes.worker.js';
+import Worker from './SlopesCanvas.worker.js';
 
 const worker = new Worker();
 
@@ -18,41 +18,18 @@ type Props = {
   polarAmount: number,
 };
 
-const Slopes = ({
+const useCanvasDrawing = (
+  canvasRef,
+  devicePixelRatio,
   width,
   height,
-  perspective,
-  spikyness,
-  polarAmount,
-}: Props) => {
+  params
+) => {
   const topMargin = (height / 11) * 1;
   const leftMargin = (width / 8.5) * 1;
   const samplesPerRow = Math.ceil(width * 0.5);
 
-  // On mount, set up the worker message-handling
-  useEffect(() => {
-    // If the browser supports it, we want to allow the canvas to be painted
-    // off of the main thread.
-    canvasRef.current = canvasRef.current.transferControlToOffscreen();
-
-    // worker.onmessage = function({ data }) {
-    //   const context = canvasRef.current;
-
-    //   if (!data.lines) {
-    //     return;
-    //   }
-
-    //   renderPolylines(data.lines, {
-    //     width,
-    //     height,
-    //     context,
-    //   });
-    // };
-
-    return () => {
-      // TODO: cleanup
-    };
-  }, []);
+  const hasSentCanvas = useRef(false);
 
   // The user can tweak "high-level parameters" like spikyness, perspective,
   // etc. These values need to be reduced to low-level variables used in
@@ -66,61 +43,89 @@ const Slopes = ({
     polarRatio,
   } = transformParameters({
     height,
+    ...params,
+  });
+
+  // On mount, set up the worker message-handling
+  useEffect(() => {
+    // If the browser supports it, we want to allow the canvas to be painted
+    // off of the main thread.
+    canvasRef.current = canvasRef.current.transferControlToOffscreen();
+  }, []);
+
+  // NOTE: Right now, I'm allowing the canvas to redraw whenever it rerenders.
+  // This seems to be OK since SlopesCanvas is a pure component (using
+  // React.memo).
+  // If this component re-renders too much, we may wish to add an array of
+  // derived variables, to only update on them
+  // eg. triggerUpdateOn = [distanceBetweenRows, perlinRatio, ...]
+  const triggerUpdateOn = undefined;
+
+  useEffect(() => {
+    let messageData = {
+      width,
+      height,
+      margins: [topMargin, leftMargin],
+      distanceBetweenRows,
+      perlinRatio,
+      rowHeight,
+      samplesPerRow,
+      polarRatio,
+    };
+    let transferable = undefined;
+
+    // If this is the very first time we're painting to the canvas, we need
+    // to send it along, using the cumbersome "data and transfer" API.
+    // More info: https://developers.google.com/web/updates/2018/08/offscreen-canvas
+    if (!hasSentCanvas.current) {
+      messageData.canvas = canvasRef.current;
+      messageData.devicePixelRatio = devicePixelRatio;
+      transferable = [canvasRef.current];
+
+      hasSentCanvas.current = true;
+    }
+
+    worker.postMessage(messageData, transferable);
+  }, triggerUpdateOn);
+};
+
+const Slopes = ({
+  width,
+  height,
+  perspective,
+  spikyness,
+  polarAmount,
+}: Props) => {
+  const canvasRef = useRef(null);
+
+  const params = {
     perspective,
     spikyness,
     polarAmount,
-  });
+  };
 
-  const canvasRef = useRef(null);
+  const devicePixelRatio = window.devicePixelRatio || 1;
 
-  const hasSentCanvas = useRef(false);
+  useCanvasDrawing(canvasRef, devicePixelRatio, width, height, params);
 
-  useEffect(
-    () => {
-      if (hasSentCanvas.current) {
-        worker.postMessage({
-          width,
-          height,
-          margins: [topMargin, leftMargin],
-          distanceBetweenRows,
-          perlinRatio,
-          rowHeight,
-          samplesPerRow,
-          polarRatio,
-        });
-      } else {
-        worker.postMessage(
-          {
-            width,
-            height,
-            margins: [topMargin, leftMargin],
-            distanceBetweenRows,
-            perlinRatio,
-            rowHeight,
-            samplesPerRow,
-            polarRatio,
-            canvas: canvasRef.current,
-            devicePixelRatio: window.devicePixelRatio,
-          },
-          [canvasRef.current]
-        );
-
-        hasSentCanvas.current = true;
-      }
-    },
-    [perspective, perlinRatio, polarRatio]
-  );
+  // HACK: We need to scale our canvas by our devicePixelRatio. This is a 2-step
+  // process:
+  //  - Change the width/height/style.width/style.height
+  //  - Use the canvas context to scale it accordingly.
+  //
+  // I normally do both of these things in the same place, but because we're
+  // using an offscreenCanvas, we don't have access to the canvas context here.
+  // So I need to do that first step inline, and trust that the ctx.scale call
+  // will exist in `SlopesCanvas.worker.js`.
 
   return (
-    <>
-      <canvas
-        width={width * 2}
-        height={height * 2}
-        style={{ width, height }}
-        ref={canvasRef}
-      />
-    </>
+    <canvas
+      width={width * devicePixelRatio}
+      height={height * devicePixelRatio}
+      style={{ width, height }}
+      ref={canvasRef}
+    />
   );
 };
 
-export default Slopes;
+export default React.memo(Slopes);
---
+++ src/components/Slopes/SlopesCanvas.worker.js
@@ -0,0 +1,41 @@
+import { throttle } from '../../utils';
+
+import { renderPolylines } from '../../vendor/polylines';
+
+import generator from './Slopes.generator';
+
+// The offscreenCanvas API doesn't like when we try to pass the canvas multiple
+// times between worker and main thread.
+//
+// The very first message received will include data.canvas and
+// data.devicePixelRatio, and we'll use those to prepare the `ctx`, which will
+// be stored in this variable:
+let ctx;
+
+onmessage = throttle(function({ data }) {
+  const { canvas, devicePixelRatio, ...lineData } = data;
+
+  const lines = generator(data);
+
+  const isFirstMessage = !ctx;
+
+  if (isFirstMessage) {
+    ctx = canvas.getContext('2d');
+
+    // NOTE: Normally, scaling the canvas is a 2-part job:
+    // - Multiply the width/height of the canvas by devicePixelRatio
+    // - Scale the context to match
+    //
+    // Because this is running off the main thread, we don't have access to
+    // `canvas.width` or `canvas.style.width`, so we can only do that second
+    // part. We're trusting SlopesCanvas.js to also take devicePixelRatio into
+    // account.
+    ctx.scale(devicePixelRatio, devicePixelRatio);
+  }
+
+  renderPolylines(lines, {
+    width: lineData.width,
+    height: lineData.height,
+    context: ctx,
+  });
+}, 20);
