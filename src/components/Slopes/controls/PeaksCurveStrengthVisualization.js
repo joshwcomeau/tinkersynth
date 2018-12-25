1a58f4c8585c823590fdced96f1eff0807406316
Joshua Comeau <joshwcomeau@gmail.com>
2018-12-25 06:00:22 -0500

    Create new, better visualization

--- src/components/Slopes/controls/PeaksCurveStrengthVisualization.js
+++ src/components/Slopes/controls/PeaksCurveStrengthVisualization.js
@@ -5,26 +5,99 @@ import { COLORS } from '../../../constants';
 import { normalize } from '../../../utils';
 
 const PeaksCurveStrengthVisualization = ({ value, size }) => {
-  const startX = normalize(value, 0, 100, 16, 0);
-  const endX = normalize(value, 0, 100, 20, 36);
+  const bodyRx = normalize(value, 0, 100, 0.5, 8);
+  const bodyRy = normalize(value, 0, 100, 5, 8.5);
+
+  const leftLegPoints = {
+    x1: normalize(value, 0, 100, 13.5528, 11.5528),
+    y1: 29.6584,
+    x2: normalize(value, 0, 100, 17.6584, 15.6584),
+    y2: 21.4472,
+  };
+
+  const rightLegPoints = {
+    x1: normalize(value, 0, 100, 22.4472, 24.4472),
+    y1: 29.6584,
+    x2: normalize(value, 0, 100, 18.3416, 20.3416),
+    y2: 21.4472,
+  };
+
+  const leftArmPoints = {
+    x1: normalize(value, 0, 100, 12, 9.38675),
+    y1: normalize(value, 0, 100, 18.5858, 10.7226),
+    x2: normalize(value, 0, 100, 16.5858, 13.7227),
+    y2: normalize(value, 0, 100, 14, 13.6132),
+  };
+
+  const rightArmPoints = {
+    x1: normalize(value, 0, 100, 23.5906, 26.872),
+    y1: normalize(value, 0, 100, 18.8829, 10.4084),
+    x2: normalize(value, 0, 100, 19.3662, 22.4084),
+    y2: normalize(value, 0, 100, 13.8927, 14.128),
+  };
 
   return (
     <Wrapper style={{ width: size, height: size }}>
-      <svg
-        width={size * 0.6}
-        height={size * 0.6}
-        viewBox="0 0 36 36"
-        style={{ overflow: 'visible' }}
-      >
+      <svg width={size - 4} height={size - 4} viewBox="0 0 36 36" fill="none">
+        {/* Left leg */}
+        <line
+          {...leftLegPoints}
+          stroke={COLORS.green[300]}
+          stroke-width="2"
+          stroke-linecap="round"
+        />
+        {/* Right leg */}
+        <line
+          {...rightLegPoints}
+          stroke={COLORS.green[300]}
+          stroke-width="2"
+          stroke-linecap="round"
+        />
+
+        {/* Left arm */}
+        <line
+          {...leftArmPoints}
+          stroke={COLORS.green[300]}
+          stroke-width="2"
+          stroke-linecap="round"
+        />
+        {/* Right arm */}
+        <line
+          {...rightArmPoints}
+          stroke={COLORS.green[300]}
+          stroke-width="2"
+          stroke-linecap="round"
+        />
+
+        {/* Body */}
+        <ellipse
+          cx="18"
+          cy="17"
+          rx={bodyRx}
+          ry={bodyRy}
+          fill={COLORS.gray[900]}
+          stroke={COLORS.green[300]}
+          strokeWidth={2}
+          style={{ opacity: value === 0 ? 0 : 1 }}
+        />
+
+        <line
+          x1={18}
+          y1={13}
+          x2={18}
+          y2={23}
+          stroke={COLORS.green[300]}
+          strokeWidth={2}
+          style={{ opacity: value === 0 ? 1 : 0 }}
+        />
+
+        {/* Head */}
         <path
-          d={`
-            M ${startX} 36
-            Q 18 -36, ${endX} 36
-          `}
-          fill="none"
-          stroke={COLORS.yellow[500]}
-          strokeWidth={3}
-          strokeLinecap="round"
+          d="M20.6643 11.5275C18.5522 13.0041 15.8999 12.5663 14.6844 10.8275C13.4688 9.08867 13.9685 6.44741 16.0807 4.97084C18.1928 3.49428 20.8451 3.93207 22.0606 5.67089C23.2762 7.40971 22.7765 10.051 20.6643 11.5275Z"
+          fill={COLORS.gray[900]}
+          stroke={COLORS.green[300]}
+          stroke-width="2"
+          transform={`translate(0 ${(value / 100) * -3})`}
         />
       </svg>
     </Wrapper>
