42d8e0b14dfbac5d99da95de8d7a48245dfc6c02
Joshua Comeau <joshwcomeau@gmail.com>
2019-01-06 09:17:19 -0500

    Start work on loading indicator

--- src/pages/slopes/index.js
+++ src/pages/slopes/index.js
@@ -3,17 +3,19 @@ import React from 'react';
 import loadable from '@loadable/component';
 
 import Layout from '../../components/Layout';
+import LoadingMachine from '../../components/LoadingMachine';
 
 const Slopes = loadable(() => import('../../components/Slopes'));
 
 const SlopesIndex = () => (
   <Layout>
-    <Slopes
+    {/* <Slopes
       fallback={
         // TODO: Custom spinner
-        <p>Building the Machine</p>
+        <LoadingMachine />
       }
-    />
+    /> */}
+    <LoadingMachine />
   </Layout>
 );
 
