c20113b64b1eb338faa942237573100810eb17f3
Joshua Comeau <joshwcomeau@gmail.com>
2019-02-10 10:40:07 -0500

    Try to use dotenv for env vars

--- src/server/config/index.js
+++ src/server/config/index.js
@@ -1,5 +1,9 @@
 // All hardcoded values are meant to be used in dev.
 // In production, the values should be set in the environment.
+const path = require('path');
+
+require('dotenv').config({ path: path.resolve(process.cwd(), '../.env') });
+
 const config = {
   STRIPE_SECRET_KEY:
     process.env.TS_STRIPE_SECRET_KEY || 'sk_test_e8GtJPEUD8WNtgv0LQA5TIsD',
