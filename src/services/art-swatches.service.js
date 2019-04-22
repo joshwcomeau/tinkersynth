90b6131d27061e4d62c02c1adb28da169e3b9581
Josh Comeau <joshwcomeau@gmail.com>
2019-04-22 17:05:26 -0400

    Add more color swatches

--- src/services/art-swatches.service.js
+++ src/services/art-swatches.service.js
@@ -1,51 +1,5 @@
 import type { SwatchData } from '../types';
 
-const ballPositions = {
-  'default-white-on-black': {
-    '#FFFFFF': {
-      xRatio: 0,
-      yRatio: 0,
-      sizeRatio: 0.6,
-    },
-  },
-  'inverted-black-on-white': {
-    '#000000': {
-      xRatio: 0,
-      yRatio: 0,
-      sizeRatio: 0.6,
-    },
-  },
-  'red-aqua': {
-    '#FF0000': {
-      xRatio: -0.1,
-      yRatio: 0.05,
-      sizeRatio: 0.6,
-    },
-    '#00FFFF': {
-      xRatio: 0.15,
-      yRatio: -0.1,
-      sizeRatio: 0.45,
-    },
-  },
-  'mossy-day': {
-    '#D3F6DB': {
-      xRatio: -0.1,
-      yRatio: 0.05,
-      sizeRatio: 0.6,
-    },
-    '#92D5E6': {
-      xRatio: 0.15,
-      yRatio: -0.1,
-      sizeRatio: 0.45,
-    },
-    '#772D8B': {
-      xRatio: 0.15,
-      yRatio: -0.1,
-      sizeRatio: 0.45,
-    },
-  },
-};
-
 class Swatch {
   constructor({ id, colors, backgroundColor }) {
     this.id = id;
@@ -85,9 +39,46 @@ const ART_SWATCHES: Array<SwatchData> = [
     colors: ['#D3F6DB', '#92D5E6', '#772D8B'],
     backgroundColor: '#A1EF8B',
   }),
+  new Swatch({
+    id: 'marbles',
+    colors: ['#0066FF', '#EB00FF', '#FFE500'],
+    backgroundColor: '#000000',
+  }),
+  new Swatch({
+    id: 'lemonade',
+    colors: ['#FFE82D', '#FF21FF'],
+    backgroundColor: '#000000',
+  }),
 ];
 
 export const getSwatchById = id =>
   ART_SWATCHES.find(swatch => swatch.id === id);
 
 export default ART_SWATCHES;
+
+const ballPositions = {
+  'default-white-on-black': {
+    '#FFFFFF': { xRatio: 0, yRatio: 0, sizeRatio: 0.6 },
+  },
+  'inverted-black-on-white': {
+    '#000000': { xRatio: 0, yRatio: 0, sizeRatio: 0.6 },
+  },
+  'red-aqua': {
+    '#FF0000': { xRatio: -0.1, yRatio: 0.05, sizeRatio: 0.6 },
+    '#00FFFF': { xRatio: 0.15, yRatio: -0.1, sizeRatio: 0.45 },
+  },
+  'mossy-day': {
+    '#D3F6DB': { xRatio: -0.1, yRatio: 0.05, sizeRatio: 0.6 },
+    '#92D5E6': { xRatio: 0.15, yRatio: -0.1, sizeRatio: 0.45 },
+    '#772D8B': { xRatio: 0.15, yRatio: -0.1, sizeRatio: 0.45 },
+  },
+  marbles: {
+    '#0066FF': { xRatio: -0.15, yRatio: 0.1, sizeRatio: 0.65 },
+    '#EB00FF': { xRatio: 0.25, yRatio: 0, sizeRatio: 0.525 },
+    '#FFE500': { xRatio: 0, yRatio: -0.2, sizeRatio: 0.4 },
+  },
+  lemonade: {
+    '#FFE82D': { xRatio: -0.2, yRatio: 0, sizeRatio: 0.6 },
+    '#FF21FF': { xRatio: 0.2, yRatio: 0, sizeRatio: 0.6 },
+  },
+};
